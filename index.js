a764538ec97589c4c9e302c5e91ba639643ac74c
rathiajay2803 <rathiajay2803@gmail.com>
2024-12-31 00:49:19 +0530

    Fix: Added env variable

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const { PORT } = require('./config/server.config');
 const app = express();
 
 app.get('/home', (req, res) => {
   res.json({ msg: 'ok' });
 });
 
-app.listen(3000, function (req, res) {
+app.listen(PORT, function (req, res) {
   console.log('Server is listening at port 3000');
 });
